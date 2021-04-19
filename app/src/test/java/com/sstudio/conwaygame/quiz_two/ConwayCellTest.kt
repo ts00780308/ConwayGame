7e405d092890874c13bc836151c11d60df451693
Samuel Shu <ts00780308@gmail.com>
2021-04-19 22:26:57 +0800

    [Commit] - Refine the unit test case

--- app/src/test/java/com/sstudio/conwaygame/quiz_two/ConwayCellTest.kt
+++ app/src/test/java/com/sstudio/conwaygame/quiz_two/ConwayCellTest.kt
@@ -8,10 +8,16 @@ import org.junit.runners.JUnit4
 @RunWith(JUnit4::class)
 class ConwayCellTest {
     @Test
-    fun testConwayCell() {
+    fun testDie() {
         val cell = ConwayCell(1, 2, true)
-        assertEquals(1, cell.xIndex)
-        assertEquals(2, cell.yIndex)
+        cell.die()
+        assertEquals(false, cell.isAlive)
+    }
+
+    @Test
+    fun testReborn() {
+        val cell = ConwayCell(1, 2, false)
+        cell.reborn()
         assertEquals(true, cell.isAlive)
     }
 }
